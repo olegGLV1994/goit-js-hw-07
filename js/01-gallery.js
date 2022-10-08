64d96f5adbaef70c1bb220e4c8e035f9cf60d1c1
Oleg Gavelya <oleggavelya@gmail.com>
2022-10-08 22:25:33 +0300

    7 dz

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+const addImage = addGalleryImg(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", addImage);
+galleryEl.addEventListener("click", addClickImage);
+
+function addGalleryImg(images) {
+  return images
+    .map(({ preview, description, original }) => {
+      return `<div class='gallery__item'><a class='gallery__link' href ='${original}'><img src ='${preview}' alt = '${description}' data-source ='${original}' class ='gallery__image'></a></div>`;
+    })
+    .join("");
+}
+
+function addClickImage(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+event.preventDefault();
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+
+  galleryEl.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+}
