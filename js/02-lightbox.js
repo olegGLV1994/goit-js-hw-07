7fb482cb8cba2040b42ff90d4246e9458dce9ce5
Oleg Gavelya <oleggavelya@gmail.com>
2022-10-09 16:11:14 +0300

    7 dz end

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,36 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
 
-
 const galleryEl = document.querySelector(".gallery");
 const addImage = addGalleryImg(galleryItems);
 galleryEl.insertAdjacentHTML("beforeend", addImage);
-// galleryEl.addEventListener("click", addClickImage);
+galleryEl.addEventListener("click", addClickImage);
 
 function addGalleryImg(images) {
   return images
     .map(({ preview, description, original }) => {
-      return `<div class='gallery__item'><a class='gallery__link' href ='${original}'><img src ='${preview}' alt = '${description}' data-source ='${original}' class ='gallery__image'></a></div>`;
+      return `<div class='gallery__item'><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></div>`;
     })
     .join("");
 }
 
-// function addClickImage(event) {
-//   if (event.target.nodeName !== "IMG") {
-//     return;
-//   }
-//   event.preventDefault();
-//   const instance = basicLightbox.create(`
-//     <img src="${event.target.dataset.source}" width="800" height="600">
-// `);
-
-//   instance.show();
-
-//   galleryEl.addEventListener("keydown", (event) => {
-//     if (event.code === "Escape") {
-//       instance.close();
-//     }
-//   });
-// }
+function addClickImage(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  event.preventDefault();
+  let lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionType: "alt",
+    captionDelay: 250,
+  });
+}
